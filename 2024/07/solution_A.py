0615e33a4241d3b894df15f7a7e877c938fcf410
Josef Toman <josef@toman.email>
2024-12-07 08:32:04 +0100

    2024/07

---
+++ 2024/07/solution_A.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def is_true(test_value, numbers):
+    if len(numbers) == 1:
+        return test_value == numbers[0]
+
+    return is_true(test_value, [numbers[0] + numbers[1], *numbers[2:]]) or is_true(test_value, [numbers[0] * numbers[1], *numbers[2:]])
+
+
+def main():
+    total = 0
+
+    for line in sys.stdin:
+        left, right = line.split(':')
+        test_value = int(left)
+        numbers = list(map(int, right.split()))
+
+        if is_true(test_value, numbers):
+            total += test_value
+    
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
